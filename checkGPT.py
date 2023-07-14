ae53ca96e3e1d9e7a77041138dfb9e6de4f4e648
Vincent Maret <vincent.maret.perso@gmail.com>
2023-07-14 22:38:26 +0200

    gpt4

--- checkGPT.py
+++ checkGPT.py
@@ -10,7 +10,7 @@ import io
 
 
 openai.organization = "org-PMp0cjQtfVV80dxgJnHY7GXJ"
-API_KEY = "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"
+API_KEY = "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"
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
 
 
@@ -38,10 +38,28 @@ def read_text_to_analyse(filename):
         line = line.replace(' ,', ',')
         line = remove_unicode_chars(line)
         processed_lines.append(line.strip())
-
     return processed_lines
 
-def generate_chat_completion(messages, model="gpt-3.5-turbo-0301", temperature=0.1, max_tokens=None):
+
+
+# query GTP4
+
+def query_gpt4(prompt,temperature=1.1,max_tokens=100):
+    messages=[{"role": "user", "content": prompt}]
+    openai.api_key = API_KEY
+    try:
+        response = openai.ChatCompletion.create(
+        model="gpt-4",
+        max_tokens=max_tokens,
+        temperature=temperature,
+        messages = messages)
+        return (True,response["choices"][0]["message"]["content"])
+        time.sleep(10)   
+    except Exception as e:
+        return (False,"")
+
+
+def query_gpt3(messages, model="gpt-3.5-turbo-0301", temperature=0.1, max_tokens=None):
     headers = {
         "Content-Type": "application/json",
         "Authorization": f"Bearer {API_KEY}",
@@ -58,7 +76,6 @@ def generate_chat_completion(messages, model="gpt-3.5-turbo-0301", temperature=0
 
     try:
         response = requests.post(API_ENDPOINT, headers=headers, data=json.dumps(data))
-
         if response.status_code == 200:
             return (True,response.json()["choices"][0]["message"]["content"])
         else:
@@ -70,24 +87,22 @@ def generate_chat_completion(messages, model="gpt-3.5-turbo-0301", temperature=0
 def is_no_error(text):
     errors = ["Le texte ne contient pas d'erreur factuelle","Il n'y a pas d'erreur factuelle dans le texte", "aucune erreur", "Il n'y a pas d'erreur factuelle","il n'y a pas d'erreur"]
     for error in errors:
-#       print (f"testing {error} against {text}")
         if error.lower() in text.lower():
-#           print ("no error ========================>")
             return True 
     return False
 
 
 def main():
-    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-#   if len(sys.argv) != 4:
-#        print(f"Usage: {sys.argv[0]} <text_file> <prompt_file> <model>")
-    if len(sys.argv) != 3:
-        print(f"Usage: {sys.argv[0]} <text_file> <prompt_file> ")
+ 
+    sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-16')
+
+    if len(sys.argv) != 4:
+        print(f"Usage: {sys.argv[0]} <text_file> <prompt_file> <model>")
         return
-    
+
     text_file = sys.argv[1]
     prompt_file = sys.argv[2]
-#    model = sys.argv[3]
+    model = sys.argv[3]
 
     paragraphs = read_text_to_analyse(text_file)
     i=0
@@ -98,15 +113,20 @@ def main():
             continue
         i=i+1
         prompt = read_file(prompt_file)
-        messages = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": f"{prompt}######{text}"} ]
+
         result=False
-        while (result == False):
-            (result,response) = generate_chat_completion(messages)
+        print (f" ==> {i}")
+        while result == False:
+            if model == "gpt3":
+                messages = [
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": f"{prompt}######{text}"} ]
+                (result,response) = query_gpt3(messages)
+            if model == "gpt4":
+                (result,response) = query_gpt4(f"{prompt}######{text}")
             if (result == False):
-                print (f"error or overload: waiting 5 seconds")
-                time.sleep(5)
+                print (f"error or overload: waiting 10 seconds")
+                time.sleep(10)
         if is_no_error(response):
             continue
         print (f"=>{text}")    
