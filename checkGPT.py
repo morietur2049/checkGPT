98e827c69e8746d0af76b9d45f8591463760df78
Vincent Maret <vincent.maret.perso@gmail.com>
2023-08-04 02:05:50 +0200

    modified to read API key from file

--- checkGPT.py
+++ checkGPT.py
@@ -9,9 +9,14 @@ import sys
 import io
 
 
-openai.organization = "org-PMp0cjQtfVV80dxgJnHY7GXJ"
-API_KEY = "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"
+# Read API_KEY and organization from keys.txt
+with open('keys.txt', 'r') as file:
+    lines = file.readlines()
+    openai.organization = lines[0].strip()
+    API_KEY = lines[1].strip()
+
 API_ENDPOINT = "https://api.openai.com/v1/chat/completions"
+
 
 
 def remove_unicode_chars(text):
@@ -44,19 +49,22 @@ def read_text_to_analyse(filename):
 
 # query GTP4
 
-def query_gpt4(prompt,temperature=1.1,max_tokens=100):
+def query_gpt4(prompt,temperature=1.1,max_tokens=None):
     messages=[{"role": "user", "content": prompt}]
     openai.api_key = API_KEY
+    sys.stderr.write ("querying gpt")
     try:
         response = openai.ChatCompletion.create(
         model="gpt-4",
         max_tokens=max_tokens,
         temperature=temperature,
         messages = messages)
+        sys.stderr.write ("answer from gpt")
         return (True,response["choices"][0]["message"]["content"])
         time.sleep(10)   
     except Exception as e:
-        return (False,"")
+        sys.stderr.write(f"####### error = {e}")
+        return (False,"")   
 
 
 def query_gpt3(messages, model="gpt-3.5-turbo-0301", temperature=0.1, max_tokens=None):
@@ -85,6 +93,7 @@ def query_gpt3(messages, model="gpt-3.5-turbo-0301", temperature=0.1, max_tokens
         return (False,"")
 
 def is_no_error(text):
+    sys.stderr.write ("entering is_no_error()")
     errors = ["Le texte ne contient pas d'erreur factuelle","Il n'y a pas d'erreur factuelle dans le texte", "aucune erreur", "Il n'y a pas d'erreur factuelle","il n'y a pas d'erreur"]
     for error in errors:
         if error.lower() in text.lower():
@@ -97,7 +106,7 @@ def main():
     sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-16')
 
     if len(sys.argv) != 4:
-        print(f"Usage: {sys.argv[0]} <text_file> <prompt_file> <model>")
+        sys.stderr.write(f"Usage: {sys.argv[0]} <text_file> <prompt_file> <model>")
         return
 
     text_file = sys.argv[1]
@@ -108,14 +117,16 @@ def main():
     i=0
     for paragraph in paragraphs:
         text=paragraph
+        sys.stderr.write (f"analysing {text}")        
         l = count_words(text)
         if (l < 10):
             continue
         i=i+1
+        sys.stderr.write (f"checking paragraph {i}")
         prompt = read_file(prompt_file)
 
         result=False
-        print (f" ==> {i}")
+        sys.stderr.write(f" ==> {i}")
         while result == False:
             if model == "gpt3":
                 messages = [
@@ -125,12 +136,17 @@ def main():
             if model == "gpt4":
                 (result,response) = query_gpt4(f"{prompt}######{text}")
             if (result == False):
-                print (f"error or overload: waiting 10 seconds")
-                time.sleep(10)
+                sys.stderr.write (f"error or overload: waiting 60 seconds")
+                time.sleep(60)
         if is_no_error(response):
             continue
+        
+        print ("")
+        sys.stderr.write ("")
+        sys.stderr.write (f"=>{text}")    
+        sys.stderr.write (f"{i} =======> {response}")
         print (f"=>{text}")    
-        print(f"{i} =======> {response}")
+        print (f"{i} =======> {response}")
         print ("")
         
 
